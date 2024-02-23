428cee9c40e0e0343a77e892ec9877aadfcf6421
Azrael7667 <silwalsolomon@gmail.com>
2024-02-23 11:41:55 +0545

    Added code

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,59 +1,56 @@
-import React from 'react'
-import aboutImg from "../assets/about4.jpeg"
+import React from 'react';
+import aboutImg from "../assets/about4.jpeg";
+
 const About = () => {
   return (
     <div className='py-10 text-white bg-[#232325] h-auto' id='about'>
       <div className='flex lg:flex-row flex-col-reverse items-center md:gap-6 gap-12 px-10 max-w-6xl mx-auto'>
 
-
         <div>
-            <div className='w-[400px]   h-full'>
-                <img src={aboutImg} alt=''
-                className='object-cover rounded-xl h-[300px]  filter grayscale brightness-50'/>
-            </div>
+          <div className='w-full h-full'>
+            <img src={aboutImg} alt=''
+              className='object-cover rounded-xl h-full w-full filter grayscale brightness-50 hover:brightness-100 transition duration-300' />
+          </div>
         </div>
 
-
         <div>
-            <div className='p-2'>
-                <div className='text-gray-300 my-3'>
-                    <h3 className='text-4xl font-semibold mb-5'>About <span>Me</span></h3>
-                    <p className='text-justify leading-7 w-11/12 mx-auto'>
-                        Hi there! I am a software engineer specializing in web development and currently working as a 
-                        I am a full stack developer with experience in building web applications using JavaScript, Node.js
-                    </p>
-                </div>
+          <div className='p-2'>
+            <div className='text-gray-300 my-3'>
+              <h3 className='text-4xl font-semibold mb-5'>About <span>Me</span></h3>
+              <p className='text-justify leading-7 w-11/12 mx-auto'>
+                Hi there! I am a software engineer specializing in web development and currently working as a 
+                I am a full stack developer with experience in building web applications using JavaScript, Node.js
+              </p>
+            </div>
+          </div>
+
+          <div className='flex mt-10 gap-7 justify-center items-center'>
+
+            <div className="bg-[#333333]/40 p-5 rounded-lg hover:bg-[#333333] cursor-pointer transition duration-300">
+              <h3 className='md:text-4xl text-2xl font-semibold text-white'>11
+                <span>+</span>
+              </h3>
+              <p><span className='md:text-base text-xs'>Projects</span></p>
             </div>
 
-            <div className='flex mt-10 gap-7 justify-center items-center'>
-
-                <div className="bg-[#333333]/40 p-5 rounded-lg">
-                    <h3 className='md:text-4xl text-2xl font-semibold text-white'>11
-                        <span>+</span>
-                    </h3>
-                <p><span className='md:text-base text-xs'>Projects</span></p>
+            <div className="bg-[#333333]/40 p-5 rounded-lg hover:bg-[#333333] cursor-pointer transition duration-300">
+              <h3 className='md:text-4xl text-2xl font-semibold text-white'>5
+                <span>+</span>
+              </h3>
+              <p><span className='md:text-base text-xs'>years of experience</span></p>
             </div>
 
-
-            <div className="bg-[#333333]/40 p-5 rounded-lg"> 
-                    <h3 className='md:text-4xl text-2xl font-semibold text-white'>5
-                        <span>+</span>
-                    </h3>
-                <p><span className='md:text-base text-xs'>years of experience</span></p>
+            <div className="bg-[#333333]/40 p-5 rounded-lg hover:bg-[#333333] cursor-pointer transition duration-300">
+              <h3 className='md:text-4xl text-2xl font-semibold text-white'>30
+                <span>+</span>
+              </h3>
+              <p><span className='md:text-base text-xs'>happy clients</span></p>
             </div>
-
-
-            <div className="bg-[#333333]/40 p-5 rounded-lg">
-                    <h3 className='md:text-4xl text-2xl font-semibold text-white'>30
-                        <span>+</span>
-                    </h3>
-                <p><span className='md:text-base text-xs'>happy clients</span></p>
-            </div>
+          </div>
         </div>
       </div>
-    </div>
     </div>
   )
 }
 
-export default About
+export default About;
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,63 +1,65 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import profilepic from '../assets/solomon.png'
 import { AiFillLinkedin, AiFillGithub, AiFillInstagram } from 'react-icons/ai'
 import { TypeAnimation } from 'react-type-animation'
-import { useState } from 'react'
-const Hero = () => {
-  
 
-  return (
-    <div>
-        <div className='my-7 sm:my-0 max-w-[1200px] h-auto mx-auto flex flex-col-reverse sm:flex-row
-                         justify-center align-center ' id='home'>
-        
-        
-        <div className=' flex-col my-auto'>
-            <p className='md:text-5xl sm:text-4xl text-xl font-bold text-gray-500 '>Hi I'm Solomon</p>
-            <h1 className='md:text-7xl sm:text-6xl text-4xl font-bold md:py-6'><TypeAnimation 
-            
-            sequence={[
-                "Frontend Dev",
-                1000,
-                "Webdesigner",
-                1000,
-                "Consultant",
-                1000,
-            ]}
-            wrapper="span"
-            speed={50}
-            repeat={Infinity}
-            />
-            </h1>
-            <div className='flex items-center justify-center '>
-                <p className='md:text-5xl sm:text4xl text-xl font-bold text-gray-500'>with a 5+ years experience</p>
-            </div>
-            <div className='text-5xl flex justify-start gap-16 my-7 text-purple-600'>
-                <AiFillLinkedin/>
-                <AiFillGithub/>
-                <AiFillInstagram/>
-            </div>
-            <div class="relative inline-flex group my-3">
-                <div class="absolute transition-all duration-1000 opacity-70 -insert-px bg-gradient-to-r
+const Hero = () => {
+
+
+    return (
+        <div>
+            <div className='my-7 sm:my-0 max-w-[1200px] h-auto mx-auto flex flex-col-reverse sm:flex-row
+                         justify-between align-center ' id='home'>
+
+
+                <div className='w-full md:w-1/2 flex-col my-auto'>
+                    <p className='md:text-5xl sm:text-4xl text-xl font-bold text-gray-500 text-center md:text-left mt-5'>Hi I'm Solomon</p>
+                    <h1 className='md:text-7xl sm:text-6xl text-center md:text-left mb-2 text-4xl font-bold md:py-6'><TypeAnimation
+
+                        sequence={[
+                            "Frontend Dev",
+                            1000,
+                            "Webdesigner",
+                            1000,
+                            "Consultant",
+                            1000,
+                        ]}
+                        wrapper="span"
+                        speed={50}
+                        repeat={Infinity}
+                    />
+                    </h1>
+                    <div className='flex items-center justify-center '>
+                        <p className='md:text-5xl sm:text4xl text-xl  md:text-left font-bold text-gray-500'>with a 5+ years experience</p>
+                    </div>
+                    <div className='text-5xl flex justify-center md:justify-start gap-16 my-7 text-purple-600'>
+                        <a href="https://www.linkedin.com/in/silwal-solomon-5bba1b27a/" target="_blank" rel="noopener noreferrer"><AiFillLinkedin /></a>
+                        <a href="https://github.com/Azrael7667" target="_blank" rel="noopener noreferrer"><AiFillGithub /></a>
+                        <a href="https://www.instagram.com/silwalsolomon/" target="_blank" rel="noopener noreferrer"><AiFillInstagram /></a>
+                    </div>
+                    <div class="relative flex justify-center md:justify-start group my-3">
+                        <div class="absolute transition-all duration-1000 opacity-70 -insert-px bg-gradient-to-r
                 from-[#44BCFF] via-[#FF44EC] to-[#FF675E] rounded-xl blur-lg group-hover:opacity-100
                 group-hover:-insert-1 group-hover:duration-200">
-                </div>
-                <a href='/'title='Download CV' role='button'
-                 class='w-[190px] h-[60px] relative inline-flex items-center px-8 py-4 text-lg
+                        </div>
+                        <a href='./Profile.pdf' title='Download CV' role='button'
+                            class='w-[190px] h-[60px] relative inline-flex items-center px-8 py-4 text-lg
                  font-bold text-white transition-all duration-200 bg-primary-color font-pj rounded-xl
                  focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900'>Download CV</a>
+                    </div>
+                </div>
+
+
+                <div className='my-auto'>
+                    <img className='w-[300px] md:w-[500px] mx-auto rounded-full mt-[-20px]' src={profilepic} alt="profile pic" />
+                </div>
+
+
             </div>
-        </div>
+            
+            </div>
 
+            );
+};
 
-        <div className='my-auto'>
-            <img className='w-[300px] sm:w-[500px] mx-auto rounded-full' src={profilepic} alt="profile pic"/>
-        </div>
-
-
-        </div>
-    </div>
-  )
-}
-
-export default Hero
+            export default Hero
